cc08ef1ac90530b50709af18680f49855922fbd1
Ralf Yang <goody80762@gmail.com>
2020-06-23 15:36:39 +0900

    Update selfservice.sh

--- selfservice.sh
+++ selfservice.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+### Code from https://github.com/ralfyang/docker-compose_for_go_revel. Powered by Github.
 cp -Rfv datadir_template datadir && chown 1000:1000 datadir
 docker-compose up -d
